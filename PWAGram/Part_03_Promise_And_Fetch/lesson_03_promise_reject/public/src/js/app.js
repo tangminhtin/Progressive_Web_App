862a735ee4dc977453167dc9cc91972c95a8918c
tangminhtin <minhtintang@gmail.com>
2020-08-17 12:25:26 +0700

    Handler promise reject by function

---
+++ PWAGram/Part_03_Promise_And_Fetch/lesson_03_promise_reject/public/src/js/app.js
@@ -0,0 +1,41 @@
+var deferredPrompt;
+
+// check browser is supported service worker or not
+if('serviceWorker' in navigator) {
+    // register new service worker
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(function() {
+            console.log('Service Worker Registered!');
+        });
+}
+
+// chrome won't show the banner
+window.addEventListener('beforeinstsllprompt', function(event) {
+    console.log('beforeinstsllprompt fired');
+    deferredPrompt = event.preventDefault();
+    return false;
+});
+
+// promise
+var promise = new Promise(function(resolve, reject) {
+    // async code in javascript
+    setTimeout(() => {
+        // resolve('This is executed once the timer is done!');
+        reject({code: 500, message: 'An error occurred!'});
+        // console.log('This is executed once the timer is done!');
+    }, 3000);
+});
+
+promise
+    .then(function(text) {
+        return text;
+    }, function(err) {
+        // handler error of reject
+        console.log(err.code, err.message);
+    })
+    .then(function(newText) {
+        console.log(newText);
+    });
+
+console.log('This is executed right after setTimeout()');
