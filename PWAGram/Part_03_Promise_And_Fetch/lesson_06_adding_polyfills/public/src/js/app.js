28dfc458255131638829f9839b3b68c9dc0788dc
tangminhtin <minhtintang@gmail.com>
2020-08-17 16:22:02 +0700

    Adding polyfills

---
+++ PWAGram/Part_03_Promise_And_Fetch/lesson_06_adding_polyfills/public/src/js/app.js
@@ -0,0 +1,112 @@
+var deferredPrompt;
+
+// check if browser not support Promise then use local promise.js file
+if(!window.Promise) {
+    window.Promise = Promise;
+}
+
+// check browser is supported service worker or not
+if('serviceWorker' in navigator) {
+    // register new service worker
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(function() {
+            console.log('Service Worker Registered!');
+        })
+        // handler reject promise service worker 
+        .catch(function(err) { 
+            console.log(err);
+        });
+}
+
+// chrome won't show the banner
+window.addEventListener('beforeinstsllprompt', function(event) {
+    console.log('beforeinstsllprompt fired');
+    deferredPrompt = event.preventDefault();
+    return false;
+});
+
+// promise
+var promise = new Promise(function(resolve, reject) {
+    // async code in javascript
+    setTimeout(() => {
+        // resolve('This is executed once the timer is done!');
+        reject({code: 500, message: 'An error occurred!'});
+        // console.log('This is executed once the timer is done!');
+    }, 3000);
+});
+
+// using ajax to get request
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+    console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+    console.log('Error!');
+};
+
+xhr.send();
+
+// fetch get request
+fetch('http://httpbin.org/ip')
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+// fetch post request
+fetch('http://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    mode: 'cors', // Access-Control-Allow-Origin
+    body: JSON.stringify({message: 'Does this work?'})
+})
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+// handler reject promise by using second argument
+// promise
+//     .then(function(text) {
+//         return text;
+//     }, function(err) {
+//         // handler error of reject
+//         console.log(err.code, err.message);
+//     })
+//     .then(function(newText) {
+//         console.log(newText);
+//     });
+
+// handler reject promise by using catch
+promise
+    .then(function(text) {
+        return text;
+    })
+    .then(function(newText) {
+        console.log(newText);
+    })
+    .catch(function(err) {
+        console.log(err.code, err.message);
+    });
+
+console.log('This is executed right after setTimeout()');
